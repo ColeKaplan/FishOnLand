4f9a857b3d2bad4abb9afd6249833f83454af627
Cole Kaplan <44578832+ColeKaplan@users.noreply.github.com>
2024-12-14 14:33:22 -0500

    Camera resizing on first 3 levels and working on entry screen

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public float targetWidth = 960f;
+    public float orthographicSize = .023f;
+    public float targetBottomY = -4.8f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float aspectRatio = (float)Screen.width / Screen.height;
+        Camera.main.orthographicSize = orthographicSize * targetWidth / (2f * aspectRatio);
+
+
+        float halfHeight = Camera.main.orthographicSize;
+        // Position the camera so its bottom aligns with the targetBottomY
+        float widthShift = targetBottomY + halfHeight; 
+
+
+
+        Camera.main.transform.position = new Vector3(0,widthShift,-20);
+    }
+}
