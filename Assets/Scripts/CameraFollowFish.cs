de863d7849d64078db1072ef2b89eb6f51d930b9
Cole Kaplan <44578832+ColeKaplan@users.noreply.github.com>
2024-12-14 16:52:08 -0500

    better sizing for following the fish in entrance and exit scenes

--- Assets/Scripts/CameraFollowFish.cs
+++ Assets/Scripts/CameraFollowFish.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CameraFollowFish : MonoBehaviour
 {
+    public GameObject fish;
+    public float targetWidth = 960f;
+    public float orthographicSize = .023f;
+    public float yShift = -6f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,12 @@ public class CameraFollowFish : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float aspectRatio = (float)Screen.width / Screen.height;
+        Camera.main.orthographicSize = orthographicSize * targetWidth / (2f * aspectRatio);
+
+
+        float halfHeight = Camera.main.orthographicSize;
+        float shift = fish.transform.position.y + halfHeight + yShift; // Maybe change 10 to 10 * screen.height / 800 or something
+        Camera.main.transform.position = new Vector3(fish.transform.position.x, shift, -10);
     }
 }
